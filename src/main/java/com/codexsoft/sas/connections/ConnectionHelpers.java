aa5091f6a42d2a40932d150dae5b0fb4ec30271a
TalgatN <developer.talgat@gmail.com>
2021-01-04 02:40:56 +0000

    Revert "added  getConnectionProperties() to ConnectionHelpers.class"
    This reverts commit fad283a680ebffb0ba69d4342442e15f5801a82c.

--- src/main/java/com/codexsoft/sas/connections/ConnectionHelpers.java
+++ src/main/java/com/codexsoft/sas/connections/ConnectionHelpers.java
@@ -1,37 +1,30 @@
 package com.codexsoft.sas.connections;
 
-import com.codexsoft.sas.config.models.ProxyConfigModel;
 import com.codexsoft.sas.connections.iom.IOMConnection;
 import com.codexsoft.sas.connections.iom.dao.RepositoriesDao;
 import com.codexsoft.sas.connections.iom.dao.ServersDao;
 import com.codexsoft.sas.connections.jdbc.JDBCConnection;
 import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Component
 @Scope("singleton")
 public class ConnectionHelpers {
-    private final ApplicationContext context;
-    private final ProxyConfigModel proxyConfigModel;
+    @Autowired
+    ApplicationContext context;
 
     @Value("${proxy.default_workspace_server_name}")
     String defaultWorkspaceServerName;
 
-    public ConnectionHelpers(ApplicationContext context, ProxyConfigModel proxyConfigModel) {
-        this.context = context;
-        this.proxyConfigModel = proxyConfigModel;
-    }
-
     public ConnectionProperties getWorkspaceConnectionPropsByServerName(
             ConnectionProperties iomConnectionProps,
             String serverName,
-            String repositoryName) throws Exception {
-
+            String repositoryName
+    ) throws Exception {
         if (serverName == null) {
             serverName = defaultWorkspaceServerName;
         }
@@ -94,11 +87,5 @@ public class ConnectionHelpers {
                 iomConnectionProps.getPassword()
         );
         return new JDBCConnection(workspaceConnectionProps);
-    }
-
-    public ConnectionProperties getConnectionProperties(HttpServletRequest request) throws Exception {
-        String serverName = request.getParameter("serverName");
-        String userName = request.getParameter("userName");
-        return proxyConfigModel.getConnection(serverName, userName);
     }
 }
