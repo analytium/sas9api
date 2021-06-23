aee8a82b1a29640279dff48146dee7f0035c5798
Hantsy Bai <hantsy@gmail.com>
2021-06-23 13:21:30 +0800

    chore: add project files to gitignore, change log config, clean pom.xml

--- jars/install.sh
+++ jars/install.sh
@@ -1,12 +1,19 @@
-mvn install:install-file -Dfile=sas.oma.joma.jar -DgroupId=sas -DartifactId=oma.joma -Dversion=9.4  -Dpackaging=jar
-mvn install:install-file -Dfile=sas.oma.omi.jar -DgroupId=sas -DartifactId=oma.omi -Dversion=9.4  -Dpackaging=jar
-mvn install:install-file -Dfile=sas.security.sspi.jar -DgroupId=sas -DartifactId=security.sspi -Dversion=9.4 -Dpackaging=jar
-mvn install:install-file -Dfile=sas.svc.connection.platform.jar -DgroupId=sas -DartifactId=sas.svc.connection.platform -Dversion=9.4  -Dpackaging=jar
-mvn install:install-file -Dfile=sas.core.jar -DgroupId=sas -DartifactId=core -Dversion=9.4 -Dpackaging=jar
-mvn install:install-file -Dfile=sas.oma.joma.rmt.jar -DgroupId=sas -DartifactId=oma.joma.rmt -Dversion=9.4  -Dpackaging=jar
-mvn install:install-file -Dfile=sas.svc.connection.jar -DgroupId=sas -DartifactId=svc.connection -Dversion=9.4 -Dpackaging=jar
-mvn install:install-file -Dfile=sas.oma.util.jar -DgroupId=sas -DartifactId=oma.util -Dversion=9.4  -Dpackaging=jar	
+#!/bin/bash
 
+mvn install:install-file -Dfile=./sas.oma.joma.jar -DgroupId=sas -DartifactId=sas.oma.joma -Dversion=9.4 -Dpackaging=jar
 
+mvn install:install-file -Dfile=./sas.oma.omi.jar -DgroupId=sas -DartifactId=sas.oma.omi -Dversion=9.4 -Dpackaging=jar
 
+mvn install:install-file -Dfile=./sas.security.sspi.jar -DgroupId=sas -DartifactId=sas.security.sspi -Dversion=9.4 -Dpackaging=jar
 
+mvn install:install-file -Dfile=./sas.svc.connection.platform.jar -DgroupId=sas -DartifactId=sas.svc.connection.platform -Dversion=9.4 -Dpackaging=jar
+
+mvn install:install-file -Dfile=./sas.core.jar -DgroupId=sas -DartifactId=sas.core -Dversion=9.4 -Dpackaging=jar
+
+mvn install:install-file -Dfile=./sas.oma.joma.rmt.jar -DgroupId=sas -DartifactId=sas.oma.joma.rmt -Dversion=9.4 -Dpackaging=jar
+
+mvn install:install-file -Dfile=./sas.svc.connection.jar -DgroupId=sas -DartifactId=sas.svc.connection -Dversion=9.4 -Dpackaging=jar
+
+mvn install:install-file -Dfile=./sas.oma.util.jar -DgroupId=sas -DartifactId=sas.oma.util -Dversion=9.4 -Dpackaging=jar
+
+echo "===================DONE========================="
